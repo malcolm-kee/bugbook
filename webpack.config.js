64d3a68027b5f347541b1fa3d349db03d87b775d
Malcolm Kee <malcolm.keeweesiong@gmail.com>
2019-02-15 12:37:21 +0800

    add webpack build process

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+const webpack = require('webpack');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const outputPath = path.resolve(__dirname, 'build');
+
+/**
+ * This webpack configuration is intentionally unoptimized so it's easier to debug for beginner
+ * Please do NOT do this in your production site
+ * @type {webpack.Configuration}
+ */
+module.exports = {
+  mode: 'development',
+  entry: {
+    app: path.resolve(__dirname, 'src', 'index')
+  },
+  output: {
+    path: outputPath,
+    publicPath: '/'
+  },
+  devtool: 'inline-source-map',
+  plugins: [
+    new CleanWebpackPlugin(outputPath, { verbose: true }),
+    new HtmlWebpackPlugin({
+      template: 'public/index.html'
+    }),
+    new CopyWebpackPlugin([
+      { from: 'public', ignore: 'index.html', to: outputPath }
+    ])
+  ]
+};
