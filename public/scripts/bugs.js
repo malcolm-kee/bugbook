f776bea7d86889b0a175f82d0d590c9d7aad39d1
Malcolm Kee <malcolm.keeweesiong@gmail.com>
2019-06-22 01:04:31 +0800

    add bug form

--- public/scripts/bugs.js
+++ public/scripts/bugs.js
@@ -1,18 +1,40 @@
 $(function() {
+  function appendBug(bug) {
+    $('#bugs-container').prepend(`
+    <article class="card">
+    <div class="card-title">${bug.status}</div>
+    <div class="card-content">
+    <p>${bug.title}</p>
+    <i>Reported by ${bug.reportedBy}</i>
+    </div>
+    </article>`);
+  }
+
   $.ajax('https://bugbook-server.herokuapp.com/bugs').done(function(bugs) {
     setTimeout(function() {
       $.each(bugs, function(_, bug) {
-        $('#bugs-container').append(`
-        <article class="card">
-        <div class="card-title">${bug.status}</div>
-        <div class="card-content">
-        <p>${bug.title}</p>
-        <i>Reported by ${bug.reportedBy}</i>
-        </div>
-        </article>`);
+        appendBug(bug);
       });
 
       $('#bugs-container .spinner').hide();
     }, 1000);
+  });
+
+  $('#bug-form').on('submit', function(ev) {
+    ev.preventDefault();
+
+    var data = {};
+
+    $.each($(this).serializeArray(), function(_, e) {
+      data[e.name] = e.value;
+    });
+
+    $.post('https://bugbook-server.herokuapp.com/bugs', data, function(newBug) {
+      appendBug(newBug);
+
+      $('#reportedBy').val('');
+      $('#status').val('');
+      $('#title').val('');
+    });
   });
 });
