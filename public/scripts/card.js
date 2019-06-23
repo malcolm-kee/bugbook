7b4156bb5200c169357edd59b1f119d74248f8ce
Malcolm Kee <malcolm.keeweesiong@gmail.com>
2019-06-23 15:18:44 +0800

    add modal for focus post

--- public/scripts/card.js
+++ public/scripts/card.js
@@ -1,25 +1,54 @@
 $(document).ready(function() {
-  function appendCard(card) {
-    $('.load-more-btn').before(`
-    <article class="card">
+  function appendPost(post) {
+    $('.load-more-btn')
+      .before(
+        `
+    <article class="card post">
       <div class="card-title">
-        <a href="${card.author.src}">${card.author.name}</a>
+        <a href="${post.author.src}">${post.author.name}</a>
+        <button class="focus-btn" type="button">...</a>
       </div>
       <div class="card-content">
-      ${card.post.text}
+      ${post.post.text}
       </div>
       ${
-        card.post.image
-          ? `<div class="card-image-container"><img class="card-image" src="${card.post.image.src}" alt="${card.post.image.alt}" /></div>`
+        post.post.image
+          ? `<div class="card-image-container"><img class="card-image" src="${post.post.image.src}" alt="${post.post.image.alt}" /></div>`
           : ''
       }
       <div class="card-actions">
-          <button type="button" class="button">
-          Like
-          </button>
+          <button type="button" class="button">Like</button>
       </div>
     </article>
-    `);
+    `
+      )
+      .prev()
+      .data('post', post);
+  }
+
+  function focusPost(post) {
+    $('#focusPost')
+      .show()
+      .find('.post-content').html(`
+    <div>
+      <a href="${post.author.src}">${post.author.name}</a>
+    </div>
+    <div class="card-content">
+      ${post.post.text}
+    </div>
+    ${
+      post.post.image
+        ? `<div class="card-image-container"><img class="modal-image" src="${post.post.image.src}" alt="${post.post.image.alt}" /></div>`
+        : ''
+    }
+      `);
+
+    $('body').addClass('modal-open');
+  }
+
+  function closeModal() {
+    $('#focusPost').hide();
+    $('body').removeClass('modal-open');
   }
 
   var getPosts = (function() {
@@ -36,7 +65,7 @@ $(document).ready(function() {
           '&_limit=' +
           postPageLimit
       ).done(function(cards) {
-        cards.forEach(appendCard);
+        cards.forEach(appendPost);
         $('.spinner').hide();
 
         if (cards.length >= postPageLimit) {
@@ -61,4 +90,11 @@ $(document).ready(function() {
     e.preventDefault();
     getPosts();
   });
+
+  $('.feed').on('click', '.focus-btn', function() {
+    var $post = $(this).closest('.post');
+    focusPost($post.data('post'));
+  });
+
+  $('#focusPost .close').on('click', closeModal);
 });
